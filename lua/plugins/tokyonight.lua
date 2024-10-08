9a2d4bada68d8852958ef710dd1fbc50b0ebe8f9
Harrizon Soler <harryalex0281@hotmail.com>
2024-10-08 01:57:52 -0500

    Add theme change keymaps

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -5,5 +5,8 @@ return {
 	config = function()
 		vim.cmd.colorscheme("tokyonight-night")
 		vim.cmd.hi("Comment gui=none")
+
+        vim.keymap.set('n', '<leader>tl', ":colorscheme tokyonight-day<CR>")
+        vim.keymap.set('n', '<leader>td', ":colorscheme tokyonight-night<CR>")
 	end,
 }
