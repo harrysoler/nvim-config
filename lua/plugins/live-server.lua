5dcb4d932216adb0f229e506ff4f54a80529941a
Harrizon <harryalex0281@hotmail.com>
2025-04-27 16:14:54 -0500

    Fix live-server plugin build typo

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -2,7 +2,7 @@ return {
 	"barrett-ruth/live-server.nvim",
 	build = [[
     if ! live-server &>/dev/null; then
-        npm install -g live-server"
+        npm install -g live-server
     fi
     ]],
 	cmd = { "LiveServerStart", "LiveServerStop" },
